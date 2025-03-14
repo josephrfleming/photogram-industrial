7dda7d15df6a8db62362445d76a29054aff4458f
Joseph Fleming <162403379+josephrfleming@users.noreply.github.com>
2025-03-14 03:23:22 +0000

    Edit Devise users migration to include default values, unique indexes, and citext

--- db/migrate/20250314025416_devise_create_users.rb
+++ db/migrate/20250314025416_devise_create_users.rb
@@ -1,3 +1,5 @@
+# db/migrate/<timestamp>_devise_create_users.rb
+
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.1]
@@ -14,35 +16,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      ## [Optional Devise modules omitted for brevity]
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-      t.string :username
-      t.boolean :private
-      t.integer :likes_count
-      t.integer :comments_count
+      # Your custom columns
+      t.string  :username
+      t.boolean :private,         default: false
+      t.integer :likes_count,     default: 0
+      t.integer :comments_count,  default: 0
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :username,             unique: true  # <--- Added!
   end
 end

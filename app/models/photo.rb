b91f0b2f638684a29d4d140b89f215d55222d5fe
Joseph Fleming <162403379+josephrfleming@users.noreply.github.com>
2025-03-14 04:54:30 +0000

    add photo scopes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,24 +1,3 @@
-# == Schema Information
-#
-# Table name: photos
-#
-#  id             :bigint           not null, primary key
-#  caption        :text
-#  comments_count :integer          default(0)
-#  image          :string
-#  likes_count    :integer          default(0)
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  owner_id       :bigint           not null
-#
-# Indexes
-#
-#  index_photos_on_owner_id  (owner_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (owner_id => users.id)
-#
 # app/models/photo.rb
 
 class Photo < ApplicationRecord
@@ -29,4 +8,8 @@ class Photo < ApplicationRecord
 
   validates :caption, presence: true
   validates :image, presence: true
+
+  # Scopes
+  scope :past_week, -> { where(created_at: 1.week.ago..Time.current) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 end

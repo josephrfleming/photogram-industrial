fc380ffe473f5d7354c5e7f41ee37573a9b85bc2
Joseph Fleming <162403379+josephrfleming@users.noreply.github.com>
2025-03-14 03:35:01 +0000

    Generated photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,13 @@
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
+# app/models/user.rb
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
 end

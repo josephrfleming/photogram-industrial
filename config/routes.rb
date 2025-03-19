09c5dc4ed5e3ef3f165364ce57af61edf0741bf3
Joseph Fleming <162403379+josephrfleming@users.noreply.github.com>
2025-03-19 06:32:27 +0000

    updated photos/_photo to use bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
-  # This route maps a URL like "/alice" to the users#show action.
-  get "/:username" => "users#show", as: :user
+  # Friendly routes for user profile collections.
+  get "/:username/liked"     => "users#liked", as: :liked
+  get "/:username/feed"      => "users#feed",  as: :feed
+  get "/:username/followers" => "users#followers", as: :followers
+  get "/:username/following" => "users#following", as: :following
+
+  # This should remain last (fallback route for a user profile)
+  get "/:username"           => "users#show", as: :user
 end
